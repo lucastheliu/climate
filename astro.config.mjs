b522274d7f8740ea586f9c821836e1a248226391
LucasTL <lucasliuhaolin@gmail.com>
2025-04-04 11:20:01 +0200

    feat: add tailwind and react as dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lucastheliu.github.io",
-	base: "climate",
+  site: "https://lucastheliu.github.io",
+  base: "climate",
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
